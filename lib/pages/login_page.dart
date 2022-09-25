f2b59c15db8ea361dcf3775992e815ce689666ba
divyanshu10072001 <tripathydivyanshu@iitgn.ac.in>
2022-09-25 23:38:14 +0530

    Login UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,27 +7,54 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("images/log.png", fit: BoxFit.cover, height: 300),
-          // Instead of Padding, can even insert code in it
-          // Can also use padding in the below text
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "Welcome!",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          // For text field we have text field and text form field, but form field has an advantage.
-          TextFormField(
-            decoration: const InputDecoration(
-                hintText: "Enter UserName", labelText: "UserName"),
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("images/log.png", fit: BoxFit.cover, height: 200),
+            // Instead of Padding, can even insert code in it
+            // Can also use padding in the below text
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Welcome!",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // For text field we have text field and text form field, but form field has an advantage.
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Username", labelText: "Enter Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Password", labelText: "Enter Password"),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/");
+              },
+              style: TextButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(236, 137, 131, 105),
+                  minimumSize: const Size(150, 40)),
+              child: const Text("Login"),
+            )
+          ],
+        ),
       ),
     );
   }
