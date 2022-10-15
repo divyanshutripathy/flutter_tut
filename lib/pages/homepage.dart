554f1196199d5c95a2590998e7bdd2c21387931e
divyanshutripathy <divyanshutripathy@jklu.edu.in>
2022-10-15 17:43:58 +0530

    Created List View

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
+import 'package:flutter_tut/models/catalog.dart';
 import 'package:flutter_tut/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,8 +10,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text("Mera Application"))),
-      body: const Text("Beech ka Bandar"),
+      appBar: AppBar(title: const Center(child: Text("Chor Bazar Online"))),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) =>
+                ItemWidget(item: CatalogModel.items[index])),
+      ),
       drawer: const MyDrawer(),
     );
   }
